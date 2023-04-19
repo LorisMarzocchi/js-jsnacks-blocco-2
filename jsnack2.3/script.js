26d5867a48fcd37c65c79ec6ada51250b031b2a3
LorisMarzocchi <ml77182a@gmail.com>
2023-04-19 10:49:59 +0200

    fixsnack2.3

--- jsnack2.3/script.js
+++ jsnack2.3/script.js
@@ -2,7 +2,11 @@
 
 const arr_numeri = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 let somma = 0;
-for (let i = 0; i < arr_numeri.length; i += 2 ){
+for (let i = 1; i < arr_numeri.length; i += 2 ){
+    // i = 0; i < arr_numeri.length; i++
+    // if (let i % 2 == 1){
+    //     somma += arr_numeri[i];
+    // }
     somma = somma + arr_numeri[i];
 }
 console.log(somma);
